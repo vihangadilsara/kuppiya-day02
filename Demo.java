de566579dfb70a4382fc339e5a032059e36d6a12
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-22 20:37:24 +0530

    Refactor Demo class to simplify number input and array initialization

--- Demo.java
+++ Demo.java
@@ -2,61 +2,19 @@
 import java.util.*;
 class Demo {
 	
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
-		int[] ar = new int[10];
-		
-		// ----------- Read Numbers -----------
-		System.out.print("Enter number 1: ");
-		ar[0] = input.nextInt();
-		
-		System.out.print("Enter number 2: ");
-		ar[1] = input.nextInt();
-		
-		System.out.print("Enter number 3: ");
-		ar[2] = input.nextInt();
-		
-		System.out.print("Enter number 4: ");
-		ar[3] = input.nextInt();
-		
-		System.out.print("Enter number 5: ");
-		ar[4] = input.nextInt();
-		
-		System.out.print("Enter number 6: ");
-		ar[5] = input.nextInt();
-		
-		System.out.print("Enter number 7: ");
-		ar[6] = input.nextInt();
-		
-		System.out.print("Enter number 8: ");
-		ar[7] = input.nextInt();
-		
-		System.out.print("Enter number 9: ");
-		ar[8] = input.nextInt();
-		
-		System.out.print("Enter number 10: ");
-		ar[9] = input.nextInt();
+// int x = 100;
 
+		int x;
+		x = 100;
+		System.out.println(x);
+
+		// int[] arr = new int[5];
+
+		int[] arr;
+		arr = new int[5];
+		System.out.println(arr);
 		
-		
-		
-		// ------- Find Total ---------
-		
-		int total = 0;
-		
-		total += ar[0];
-		total += ar[1];
-		total += ar[2];
-		total += ar[3];
-		total += ar[4];
-		total += ar[5];
-		total += ar[6];
-		total += ar[7];
-		total += ar[8];
-		total += ar[9];
-		
-		System.out.println("Total: " + total);
+		arr[0] = 100;
+		System.out.println(arr[0])
     }
 }
-
