aeca5334b3aa0991908fcde64dd6642d8e67b8b9
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-22 22:42:24 +0530

    Refactor Demo class to remove unused imports and improve array printing

--- Demo.java
+++ Demo.java
@@ -1,12 +1,14 @@
-
-import java.util.*;
-
-class Demo  {
+class Demo {
+	public static void main(String[] args) {
 		int[] arr = {100, 200, 300, 400, 500};
 		
 		// --------- Print Array ------------
-		
+		System.out.print("[");
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + ", ");
+		}
+		System.out.println("\b\b]");
     }
-
+}
 
 
