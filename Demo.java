e99eb3f33e963c1fec05169e8a7398b10d2242bb
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-22 23:07:42 +0530

    Refactor Demo class to improve printArray method and enhance readability

--- Demo.java
+++ Demo.java
@@ -1,14 +1,24 @@
 class Demo {
+
+	static void printArray(int[] arr){
+		System.out.print("[");
+		//for(int i=0; i<arr.length; i++) {
+		//	System.out.print(arr[i] + ", ");
+		//}
+		
+		for(int element: arr) { // For Each | Enhanced For Loop
+			System.out.print(element + ", ");
+		}
+		
+		System.out.println("\b\b]");
+	}
+	
 	public static void main(String[] args) {
 		int[] arr = {100, 200, 300, 400, 500};
 		
-		// --------- Print Array ------------
-		System.out.print("[");
-		for(int i=0; i<arr.length; i++) {
-			System.out.print(arr[i] + ", ");
-		}
-		System.out.println("\b\b]");
+		printArray(arr);
     }
+
 }
 
 
