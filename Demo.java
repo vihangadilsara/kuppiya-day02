dae4e1321c774ec98a5ba82d39396875cb229dca
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-23 22:11:52 +0530

    Refactor Demo class to remove printArray method and clean up code

--- Demo.java
+++ Demo.java
@@ -1,24 +1,7 @@
-class Demo {
+class Demo{
 
-	static void printArray(int[] arr){
-		System.out.print("[");
-		//for(int i=0; i<arr.length; i++) {
-		//	System.out.print(arr[i] + ", ");
-		//}
-		
-		for(int element: arr) { // For Each | Enhanced For Loop
-			System.out.print(element + ", ");
-		}
-		
-		System.out.println("\b\b]");
-	}
+
+
 	
-	public static void main(String[] args) {
-		int[] arr = {100, 200, 300, 400, 500};
-		
-		printArray(arr);
-    }
-
 }
-
 
